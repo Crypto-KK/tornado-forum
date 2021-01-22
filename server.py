9873a9840d4b6c6495d45dcdbbaaccf64534a58f
kk <705555262@qq.com>
2021-01-22 10:02:08 +0800

    feat(): 增加loguru日志模块

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import asyncio
 import tornado
 from tornado import web
 from tornado.options import parse_command_line
+from loguru import logger
 
 from databases.redis import RedisPool
 from forum.urls import urlpattern
@@ -15,6 +16,7 @@ import peewee_async
 
 
 def make_app(loop):
+    logger.add("logs/api.log")
     wtforms_json.init()
     apps = tornado.web.Application(urlpattern, debug=True, **settings)
     # 把loop传递过RedisPool获取一个链接
@@ -27,10 +29,11 @@ def make_app(loop):
 
 if __name__ == '__main__':
     try:
+        logger.info("server start")
         parse_command_line()
         loop = asyncio.get_event_loop()
         app = make_app(loop)
         app.listen(8888)
         loop.run_forever()
     except KeyboardInterrupt:
-        pass
+        logger.info("server stop")
